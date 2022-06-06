50d6a3f1238c6bc4afd478e6cdd413c55abaf8f6
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-06 13:23:01 +0700

    CountPrime failed

---
+++ src/CountPrime.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class CountPrime{
+    private static boolean isPrime(int n){
+        if (n<2){
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(i); i++){
+            if (n % i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void countPrime(int[] arr){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (isPrime(arr[i])){
+                count ++;
+            }
+        }
+        System.out.print(count);
+    }
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        countPrime(arr);
+        sc.close();
+
+    }
+}
