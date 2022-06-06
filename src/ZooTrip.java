528aa864bc4f830c99021a9c2ad77d75626026da
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-06 13:24:45 +0700

    ZooTrip failed

---
+++ src/ZooTrip.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class ZooTrip{
+    private static void gameHeld(int[] arr){
+        if (arr.length % 2 != 0){
+            System.out.println("NO");
+            return;
+        }
+
+        int count1 = 0;
+        int count0 = 0;
+        for(int i = 0; i<arr.length; i++){
+            if (arr[i] == 1){
+                count1++;
+            }
+            else if (arr[i] == 0){
+                count0++;
+            }
+        }
+        if (count1 != count0){
+            System.out.print("NO");
+        }
+        else{
+            System.out.print("YES");
+        }
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        gameHeld(arr);
+        sc.close();
+    }
+}
